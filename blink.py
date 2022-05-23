23c88fe5a97c6a73e685b8598c300dff75016dc2
dgct <d@tree.casa>
2022-05-22 22:19:07 -0700

    init

--- blink.py
+++ blink.py
@@ -1,271 +1,71 @@
 import numpy as np
 import scipy.sparse as sp
-
-BIOCHEM_SHIFTS = {
-    "C": 12.0,
-    "H": 1.00783,
-    "H2": 2.01566,
-    "O": 15.99491,
-    "NH2-O": 0.02381,
-    "PO3": 78.95851,
-    "S": 31.97207,
-}
-
-ISOTOPIC_SHIFTS = {
-    "13C-12C": 1.0034,
-    "D-H": 1.0063,
-    "15N-14N": 0.9970,
-    "18O-16O": 2.0042,
-}
+from scipy.stats import rankdata
+import copy
 
 
-class SparseData:
+class vector:
     def __init__(
         self,
-        x,
-        y=None,
-        x_ends=None,
-        x_kind="points",
-        y_kind="values",
-        value_power=0.5,
-        tolerance=0.01,
-        shifts=[],
-        mirror_shifts=True,
-        combination_steps=1,
-        metadata=None,
+        x=np.array([]),
+        y=np.array([]),
+        data=None,
+        x_tolerance=0.0,
+        y_tolerance=0.0,
+        x_transform=lambda x: np.array([x]),
+        y_transform=lambda y: np.array([y]),
     ):
 
-        assert (y is None) or ([xi.shape for xi in x] == [yj.shape for yj in y])
-        assert (x_ends is None) or (len(x) == len(x_ends))
+        x = np.asarray(x)
+        y = np.asarray(y)
+        if data is None:
+            data = np.ones_like(x)
+        data = np.asarray(data)
 
-        # flatten input data backfilling if necessary
-        ids = np.concatenate([[i] * len(xi) for i, xi in enumerate(x)])
-        x = np.concatenate(x)
-        if y is None:
-            y = np.ones_like(x)
-        else:
-            y = np.concatenate(y, axis=0)
-        if x_ends is not None:
-            x_ends = np.asarray(x_ends)
+        for name, var in zip(["x", "y", "data"], [x, y, data]):
+            if var.ndim != 1 or var.dtype.kind not in "biufcmM":
+                raise ValueError("{} is mishapen or non-numeric.".format(name))
 
-        # id data
-        self._id = ids
-        self.metadata = metadata
+        if not (len(x) == len(y) == len(data)):
+            raise ValueError("x, y, and data not equal length.")
 
-        # x data
-        self._points = x
-        self._x_ends = x_ends
-        self._losses = None
-        if x_kind == "points":
-            self._x = self._points
-        elif x_kind == "losses":
-            self._x = self._losses_proxy
+        self.x_tolerance = x_tolerance
+        self.y_tolerance = y_tolerance
+        self.x_transform = x_transform
+        self.y_transform = y_transform
 
-        # y data
-        self._values = y**value_power
-        self._value_power = value_power
-        self._counts = None
-        if y_kind == "values":
-            self._y = self._values
-        elif y_kind == "counts":
-            self._y = self._counts_proxy
+        idx_sort = np.argsort(y + 1j * x)
 
-        # link data
-        self._tolerance = tolerance
-        self._shifts = np.asarray(shifts)
-        self._mirror_shifts = mirror_shifts
-        self._combination_steps = combination_steps
-        self._kernel = None
-        self._norm = None
+        self.x = x_transform(x[idx_sort])
+        self.y = y_transform(y[idx_sort])
+        self.data = data[idx_sort]
 
-        self._update()
-
-    #############
-    # Properties
-    #############
-    # X
-
-    @property
-    def _losses_proxy(self):
-        if self._losses is None:
-            self._losses = self._x_ends[self._id] - self._points
-        return self._losses
-
-    @property
-    def x_kind(self):
-        if self._x is self._points:
-            return "points"
-        elif self._x is self._losses:
-            return "losses"
-
-    @x_kind.setter
-    def x_kind(self, new_x_kind):
-        if (new_x_kind == "points") and (self._x is self._losses):
-            self._x = self._points
-            self._kernel = -(self._kernel - self._x_ends[self._id])
-        elif (new_x_kind == "losses") and (self._x is self._points):
-            self._x = self._losses_proxy
-            self._kernel = self._x_ends[self._id] - self._kernel
-
-    #############
-    # Y
-
-    @property
-    def _counts_proxy(self):
-        if self._counts is None:
-            self._counts = self._values.astype(bool)
-        return self._counts
-
-    @property
-    def y_kind(self):
-        if self._y is self._values:
-            return "values"
-        elif self._y is self._counts:
-            return "counts"
-
-    @y_kind.setter
-    def y_kind(self, new_y_kind):
-        if (new_y_kind == "values") and (self._y is self._counts):
-            self._y = self._values
-            self._normalize()
-        elif (new_y_kind == "counts") and (self._y is self._values):
-            self._y = self._counts_proxy
-            self._normalize()
-
-    @property
-    def value_power(self):
-        return self._value_power
-
-    @value_power.setter
-    def value_power(self, new_value_power):
-        if self._value_power != new_value_power:
-            self._y = self._y ** (new_value_power / self._value_power)
-            self._value_power = new_value_power
-            self._normalize()
-
-    #############
-    # Link
-
-    @property
-    def tolerance(self):
-        return self._tolerance
-
-    @tolerance.setter
-    def tolerance(self, new_tolerance):
-        if self._tolerance != new_tolerance:
-            self._tolerance = new_tolerance
-            self._normalize()
-
-    @property
-    def shifts(self):
-        return self._shifts
-
-    @shifts.setter
-    def shifts(self, new_shifts):
-        if (self._shifts.shape != new_shifts.shape) or (
-            self._shifts != new_shifts
-        ).any():
-            self._shifts = new_shifts
-            self._update()
-
-    @property
-    def combination_steps(self):
-        return self._combination_steps
-
-    @combination_steps.setter
-    def combination_steps(self, new_combination_steps):
-        if self._combination_steps != new_combination_steps:
-            self._combination_steps = new_combination_steps
-            self._update()
-
-    @property
-    def mirror_shifts(self):
-        return self._mirror_shifts
-
-    @mirror_shifts.setter
-    def mirror_shifts(self, new_mirror_shifts):
-        if self._mirror_shifts != new_mirror_shifts:
-            self._mirror_shifts = new_mirror_shifts
-            self._update()
+        self._squeeze()
+        self._prune()
 
     #################
-    # Helper Methods
+    # Blink Methods
     #################
 
-    def _update(self):
-        if self._mirror_shifts:
-            shifts = np.multiply.outer(self._shifts, [1, -1]).flatten()
-        else:
-            shifts = self._shifts
+    def _blur(self, other, link):
+        diff = self.y[0, link[0]] - other.x[link[2], link[1]]
 
-        shifts = np.unique(np.append(0.0, shifts))
+        if np.isclose(diff, 0).all():
+            return 1
 
-        # recursively combine shifts within a specified number of steps
-        def combine(shifts, combination_steps):
-            if combination_steps == 0:
-                return np.array([0.0])
-            if combination_steps == 1:
-                return shifts
-            else:
-                return np.add.outer(shifts, combine(shifts, combination_steps - 1))
+        return 1 - (diff / self.y_tolerance) ** 2
 
-        shifts = np.unique(combine(shifts, self._combination_steps).flatten())
-
-        self._kernel = np.add.outer(shifts, self._x)
-
-        self._normalize()
-
-    def _normalize(self):
-        link = self._link(self, same_id=True)
-
-        link = link[:, np.argsort(self._id[link[0]])]
-
-        same_id = self._id[link[0]] == self._id[link[1]]
-
-        id_mask = self._id[link[0]][1:] != self._id[link[0]][:-1]
-        id_mask = np.append(True, id_mask)
-        (id_edge,) = np.nonzero(id_mask)
-
-        self._norm = (
-            np.add.reduceat(
-                same_id
-                * self._parabolic_blur(self, link)
-                * self._y[link[0]]
-                * self._y[link[1]],
-                id_edge,
-            )
-            ** -0.5
-        )
-
-        if self._y is self._counts:
-            self._norm *= np.add.reduceat(self._counts[link[0]], id_edge) ** 0.5
-
-    def _link(self, other, same_id=False):
-        assert self._tolerance == other.tolerance
-
-        self_x = self._x
-        other_kernel = other._kernel
-        tolerance = self._tolerance
-
-        if same_id:
-            assert self is other
-
-            self_x = 1j * self_x + self._id
-            other_kernel = 1j * other_kernel + self._id
-            tolerance = 1j * tolerance
-
-        sort_idx = np.argsort(self_x)
-
+    def _link(self, other):
         overlap = np.array(
             [
                 np.searchsorted(
-                    self_x[sort_idx],
-                    other_kernel.ravel() - tolerance,
+                    self.y[0],
+                    other.x.ravel() - self.y_tolerance,
                     "left",
                 ),
                 np.searchsorted(
-                    self_x[sort_idx],
-                    other_kernel.ravel() + tolerance,
+                    self.y[0],
+                    other.x.ravel() + self.y_tolerance,
                     "right",
                 ),
             ]
@@ -277,61 +77,246 @@ class SparseData:
 
         link = np.array(
             [
-                sort_idx[_multi_arange(overlap[:, overlap[0] != overlap[1]].T)],
-                link_idx % other_kernel.shape[1],
-                link_idx // other_kernel.shape[1],
+                _multi_arange(overlap[:, overlap[0] != overlap[1]].T),
+                link_idx % other.x.shape[1],
+                link_idx // other.x.shape[1],
             ]
         )
 
         return link
 
-    def _parabolic_blur(self, other, link):
-        x_diff = self._x[link[0]] - other._kernel[link[2], link[1]]
+    def _squeeze(self):
+        same_x = self.x[0, 1:] != self.x[0, :-1]
+        same_y = self.y[0, 1:] != self.y[0, :-1]
+        same = same_x | same_y
+        same = np.append(True, same)
+        (same_edge,) = np.nonzero(same)
 
-        return (3 / 4) * (1 - abs(x_diff / self._tolerance) ** 2)
+        self.x = self.x[:, same]
+        self.y = self.y[:, same]
+        self.data = np.add.reduceat(
+            self.data,
+            same_edge,
+        )
+
+    def _prune(self):
+        mask = self.data != 0
+        self.x = self.x[:, mask]
+        self.y = self.y[:, mask]
+        self.data = self.data[mask]
+
+    #################
+    # Operators
+    #################
+
+    def _operate(self, other, func):
+        if not isinstance(other, (float, int, complex)):
+            raise ValueError("{} is not a scalar.".format(other))
+        self.data = func(self.data, other)
+
+    def __eq__(self, other):
+        result = True
+        result &= (self.x == other.x).all()
+        result &= (self.y == other.y).all()
+        result &= (self.data == other.data).all()
+
+        return result
+
+    def __neq__(self, other):
+        result = False
+        result |= (self.x != other.x).any()
+        result |= (self.y != other.y).any()
+        result |= (self.data != other.data).any()
+
+        return result
+
+    def __add__(self, other):
+        if isinstance(other, self.__class__):
+            if not (
+                (self.x_tolerance == other.x_tolerance)
+                and (self.y_tolerance == other.y_tolerance)
+                and (self.x_transform == other.x_transform)
+                and (self.y_transform == other.y_transform)
+            ):
+                raise ValueError("tolerances and transforms not equal between vectors.")
+
+            x = np.concatenate([self.x[0], other.x[0]])
+            y = np.concatenate([self.y[0], other.y[0]])
+            data = np.concatenate([self.data, other.data])
+
+            result = self.__class__(
+                x,
+                y,
+                data,
+                self.x_tolerance,
+                self.y_tolerance,
+                self.x_transform,
+                self.y_transform,
+            )
+
+            return result
+
+        result = self.copy()
+        result._operate(other, lambda i, o: i + o)
+        return result
+
+    def __iadd__(self, other):
+        self._operate(other, lambda i, o: i + o)
+        return self
+
+    def __mul__(self, other):
+        if isinstance(other, self.__class__):
+            return self @ other
+
+        result = self.copy()
+        result._operate(other, lambda i, o: i * o)
+        return result
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __imul__(self, other):
+        self._operate(other, lambda i, o: i * o)
+        return self
+
+    def __truediv__(self, other):
+        result = self.copy()
+        result._operate(other, lambda i, o: i / o)
+        return result
+
+    def __rtruediv__(self, other):
+        return (self**-1) * other
+
+    def __itruediv__(self, other):
+        self._operate(other, lambda i, o: i / o)
+        return self
+
+    def __floordiv__(self, other):
+        result = self.copy()
+        result._operate(other, lambda i, o: i // o)
+        return result
+
+    def __ifloordiv__(self, other):
+        self._operate(other, lambda i, o: i // o)
+        return self
+
+    def __pow__(self, other):
+        result = self.copy()
+        result._operate(other, lambda i, o: i**o)
+        return result
+
+    def __ipow__(self, other):
+        self._operate(other, lambda i, o: i**o)
+        return self
+
+    def __matmul__(self, other):
+        if not isinstance(other, self.__class__):
+            raise NotImplementedError(
+                "vector multiplication only defined between blink vectors"
+            )
+        if self.y_tolerance != other.x_tolerance:
+            raise ValueError(
+                "x tolerance of right vector must equal y tolerance of left vector"
+            )
+
+        link = self._link(other)
+
+        _, linked = np.unique(link, axis=1, return_inverse=True)
+        linked = linked[1:] != linked[:-1]
+        linked = np.append(True, linked)
+        (linked_edge,) = np.nonzero(linked)
+
+        x = self.x[0, link[0]]
+        y = other.y[0, link[1]]
+        data = np.add.reduceat(
+            self._blur(other, link) * self.data[link[0]] * other.data[link[1]],
+            linked_edge,
+        )
+
+        result = self.__class__(
+            x,
+            y,
+            data,
+            self.x_tolerance,
+            other.y_tolerance,
+            self.x_transform,
+            other.y_transform,
+        )
+
+        return result
 
     #################
     # Public Methods
     #################
 
-    def score(self, other):
-        assert self.x_kind == other.x_kind
-        assert self.y_kind == other.y_kind
+    def copy(self):
+        return copy.copy(self)
 
-        assert self.tolerance == other.tolerance
-        assert (self._shifts.shape == other._shifts.shape) and (
-            self._shifts == other._shifts
-        ).all()
-        assert self.mirror_shifts == other.mirror_shifts
-        assert self.combination_steps == other.combination_steps
+    def transpose(self):
+        result = self.copy()
 
-        ordered = self._x.size <= other._x.size
-        if not ordered:
-            self, other = other, self
+        result.x, result.y = result.y, result.x
+        result.x_tolerance, result.y_tolerance = result.y_tolerance, result.x_tolerance
+        result.x_transform, result.y_transform = result.y_transform, result.x_transform
 
-        link = self._link(other)
-        x_bins = np.unique(link[0])
+        idx_sort = np.argsort(result.y[0] + 1j * result.x[0])
+        result.x = result.x[:, idx_sort]
+        result.y = result.y[:, idx_sort]
+        result.data = result.data[idx_sort]
 
-        S = sp.csr_matrix(
-            (
-                (self._norm[self._id] * self._y)[x_bins],
-                (self._id[x_bins], x_bins),
-            ),
+        return result
+
+    @property
+    def T(self):
+        return self.transpose()
+
+    def diag(self):
+        same = self.x[0] == self.y[0]
+        x = self.x[0, same]
+        y = self.y[0, same]
+        data = self.data[same]
+
+        result = self.__class__(
+            x,
+            y,
+            data,
+            self.x_tolerance,
+            self.y_tolerance,
+            self.x_transform,
+            self.y_transform,
         )
 
-        O = sp.csr_matrix(
+        return result
+
+    def norm(self):
+        result = (self @ self.T).diag()
+
+        result **= -0.5
+        result.y_tolerance = 0
+        self.x_tolerance = 0
+
+        result = result @ self
+
+        return result
+
+    def score(self, other, norm=False):
+        if norm:
+            self = self.norm()
+            other = other.norm()
+
+        return self @ other.T
+
+    def tocoo(self):
+        result = sp.coo_matrix(
             (
-                self._parabolic_blur(other, link)
-                * (other._norm[other._id] * other._y)[link[1]],
-                (link[0], other._id[link[1]]),
-            ),
+                self.data,
+                (rankdata(self.x[0], "dense") - 1, rankdata(self.y[0], "dense") - 1),
+            )
         )
+        return result
 
-        blink_score = S.dot(O)
-        if not ordered:
-            blink_score = blink_score.T
-
-        return blink_score
+    def toarray(self):
+        return self.tocoo().toarray()
 
 
 def _multi_arange(a):
